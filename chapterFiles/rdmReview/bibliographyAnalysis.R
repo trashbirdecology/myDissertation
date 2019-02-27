b14a31f2ed283cbde30ba8b075afb459259b8aa5
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-02-27 09:51:48 -0600

    Update review chap

---
+++ chapterFiles/rdmReview/bibliographyAnalysis.R
@@ -0,0 +1,13 @@
+library(revtools)
+
+revChapDir <- paste0(here::here(), "/chapterfiles/rdmReview")
+myFile <- paste0(revChapDir, "/regimeshift_20190225.bib")
+
+bib <- read_bibliography(myFile)
+class(bib)
+summary(bib)
+names(bib)
+
+x=find_duplicates(data = bib, match_variable = "label")
+screen_titles(bib)
+screen_topics(bib)
