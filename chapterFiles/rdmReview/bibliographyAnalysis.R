ed3e2ea667f7387608d0f8bae3b9cdf9b252754d
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-03-04 16:21:05 -0600

    bib updates

--- chapterFiles/rdmReview/bibliographyAnalysis.R
+++ chapterFiles/rdmReview/bibliographyAnalysis.R
@@ -2,13 +2,59 @@ require(tidyverse)
 library(revtools)
 rdm.dir <- paste0(here::here(), "/chapterfiles/rdmReview")
 
-myFile <- paste0(revChapDir, "/regimeshift_20190225.bib")
+# Load data ---------------------------------------------------------------
+# Load the WOS search results bib file
+fn <- paste0(rdm.dir, "/reviewResults/wosRdmReview_20190304.bib")
+bib.wos <- read_bibliography(fn)
 
-bib <- read_bibliography(myFile)
-class(bib)
-summary(bib)
-names(bib)
+# Load the GoogleScholar bib file
+fn <- paste0(rdm.dir, "/reviewResults/googleScholarRdmReview_20190304.bib")
+bib.gs <- read_bibliography(fn)
 
-x=find_duplicates(data = bib, match_variable = "label")
-screen_titles(bib)
-screen_topics(bib)
+# Load the GoogleScholar bib file
+fn <- paste0(rdm.dir, "/reviewResults/priorRdmReview_20190304.bib")
+bib.prior <- read_bibliography(fn)
+
+
+# Create soem filters -----------------------------------------------------
+f1 <- paste("regime", "shift", "regime shift", "regime change", "method", "new", "novel", 
+                       "abrupt", "stability", "transition", "threshold", "tipping point", "change point", "change-point", 
+                       sep = "|"
+  )
+
+t = bib.wos %>%
+  mutate(title = tolower(title), 
+         abstract = tolower(abstract)) %>% 
+  filter(str_detect(title, paste(f1))  |    
+           str_detect(abstract, paste(f1)) 
+                    )
+
+f2 <- paste("new method", "novel method", "new approach", "novel approach", "new numerical", "novel numerical", 
+            "new quantitative", "novel quantitative", "i introduce", "we introduce", 
+            sep = "|")
+
+t2 = bib.wos %>%
+  mutate(abstract = tolower(abstract)) %>% 
+  filter(str_detect(abstract, paste(f2)) 
+  )
+
+t3 = bib.wos %>%
+  mutate(title = tolower(title)) %>% 
+  filter(str_detect(title, paste(f2)) 
+  )
+
+
+
+# Munge the data ----------------------------------------------------------
+
+(bib.wos)
+
+
+# Some dissertation text -------------------------------------------------------
+
+# Final number of methods papers used for each category. Thesea re called in the dissertation so do not remove!
+n.wos <- nrow(bib.gs)
+n.google <- nrow(bib.gs)
+n.prior <- nrow(bib.prior)
+
+
