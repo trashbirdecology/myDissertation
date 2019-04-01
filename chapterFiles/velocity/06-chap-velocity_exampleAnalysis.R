006430523607e79867dc7436633305181a1a0d01
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-04-01 18:05:59 -0500

    MAjor updates. Knitted OK

--- chapterFiles/velocity/06-chap-velocity_exampleAnalysis.R
+++ chapterFiles/velocity/06-chap-velocity_exampleAnalysis.R
@@ -1,3 +1,7 @@
+# Source helper fun
+source("./chapterFiles/velocity/06-chap-velocity_helperFuns.R")
+
+
 # Simulate a simple, two variable system 
 if(!exists("mean.analy")) mean.analy=FALSE
 if(!exists("var.analy")) var.analy=FALSE
@@ -61,33 +65,6 @@ for(j in 1:1e4){ # this will take some time.
 
 
 
-# Approx results fun ------------------------------------------------------
-approx_dist.sim <- function(t.mult){
-  
-  dist<-list()
-  x_1 = c(rnorm(mean =25, sd = 5, n=50), rnorm(mean = 100, sd = 5, n=50))
-  x_2 = c(rnorm(mean =25, sd = 5, n=50), rnorm(mean = 50, sd = 5, n=50))
-  t = 1:length(x_1)
-  dist[[1]] <- data.frame(t, x_1, x_2) %>% 
-    gather(key="variable", value="value", -t)
-  
-  x_1 = approx(t, x_1, n = t.mult*length(t))
-  x_2 = approx(t, x_2, n = t.mult*length(t))
-  t=x_1$x
-  
-  df.wide = data.frame(t, x_1$y, x_2$y)
-  
-  df  = data.frame(t, x_1, x_2) %>% 
-    tidyr::gather(key = "variable", value = "value", -t) 
-  dist[[2]] <- df %>% 
-    mutate(cellID = 1) %>% 
-    rename(sortVar = t) %>% 
-    regimeDetectionMeasures::calculate_distanceTravelled() %>% 
-    dplyr::select(-cellID) %>% 
-    rename(t = sortVar)
-  
-  return(dist)
-}
 
 # End Run -----------------------------------------------------------------
 
