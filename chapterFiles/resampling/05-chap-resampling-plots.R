9e7a5138afef450322820628cef4e9a1c579e54d
Jessica Burnett <jessicaleighburnett@gmail.com>
2019-04-24 09:22:55 -0500

    Plotting updates mostly

--- chapterFiles/resampling/05-chap-resampling-plots.R
+++ chapterFiles/resampling/05-chap-resampling-plots.R
@@ -7,6 +7,7 @@ rm(list=ls())
 ## Libs
 library(tidyverse)
 library(feather)
+library(here)
 
 # Source functions
 source(paste0(here::here(),"/chapterFiles/resampling/05-chap-resampling-myFunctions.R"))
@@ -20,12 +21,72 @@ diatoms <- TRUE # do you want to analyze the Spanbuaer data? if not, program wil
 dirs <- createDirs(dirNameInd=ifelse(diatoms==TRUE, "diatoms","dummy")) # creates dirs if null and IDs
 dataDir <- dirs$summaryResultsDir
 
+# Load summarizes results ------------------------------------------------------------
+
+my.ind <- "distances"
+results.dist <- purrr::map_df(list.files(dataDir, full.names=TRUE, pattern = my.ind), read_feather)
+my.ind <- "fivi"
+results.fivi <- purrr::map_df(list.files(dataDir, full.names=TRUE, pattern = my.ind), read_feather)
+
 # Create Plots ------------------------------------------------------------
 
-my.ind <- "*distances"
+# which metric to plot
+metric.ind <- "s"
+group.ind1 <- "observations" # a grouping variable
+# group.ind1 <- "method" # a grouping variable
+group.ind2 <- "prob" # a faceting var
+n.col <- 1 # # facet cols
 
-list.files(dataDir)
+df <- results.dist
+x <- "time"
+y <-  paste0(metric.ind,".mean")
+y.sd <- paste0(metric.ind, ".sd")
 
-results.temp = purrr::map_df(list.files(dataDir, full.names=TRUE, pattern = my.ind), read_feather)
+if(exists("group.ind1")) group1 <- paste0("as.factor(", group.ind1, ")")
+if(exists("group.ind2")) group2 <- paste0("as.factor(", group.ind2, ")")
 
+
+ggplot(data = df %>% 
+         filter(
+  time > min(time), 
+  method == method.filter
+                            )) +
+  geom_line(aes_string(x = x, y = y), alpha = .6) +
+    facet_wrap(facets = as.formula(paste0("~", group2)), ncol=n.col, scales="free_y")+
+  theme_mine() +
+  theme(legend.position = "top") 
+
+df.temp <- df %>% 
+  mutate(
+    upper = !!sym(y)+1.96*!!sym(y.sd),
+    lower = !!sym(y)-1.96*!!sym(y.sd)
+  ) %>% 
+  filter(method %in% method.filter) %>% 
+  filter(!is.na(upper), 
+         !is.na(lower)) %>% 
+  mutate(prob = as.factor(100*as.numeric(as.character(prob))),
+         method=as.factor(method))
+# Change for labelling purposes
+if(group2=="as.factor(prob)" | group2=="as.factor(prob)") levels(df.temp$prob) =  paste0(levels(df.temp$prob),"%")
+
+## ribbons
+p.ribbon <-
+  ggplot(data = df.temp) +
+  geom_line(aes_string(x = x, y = y)) +
+  facet_wrap(facets = as.formula(paste0("~", group2)), ncol = n.col, scales="free_y") +
+  geom_ribbon(aes_string(x = x,
+                         ymin = 'lower',
+                         ymax = 'upper' 
+  ), na.rm = TRUE,
+  color="grey", alpha=0.3)+
+  theme_mine() +
+  theme(legend.position = "top") +
+  ylab("mean velocity")
+
+fn <- paste0()
+ggsave(plot=p.ribbon, device=".png",
+       filename=)
+
+
+  
 
