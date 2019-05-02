cf1b860ff42d21dcdc6950f79dfc9db4bc9c2a6e
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-05-02 12:45:47 -0500

    Add bootstrap diss figs and update ignroe

--- CHAPTER-ISSUES.R
+++ CHAPTER-ISSUES.R
@@ -1,5 +1,2 @@
 ## ISSUES
-04-chap-binning.RMD
-- Need to create figures for the spatial grid, the NS and EW transects along a map
-- getBBSdata is not saving to bbs_raw_data, but is saving to the binningChap folder. Not sure why. 
-    * I checked the bbsRDM function and it seems fine. Not terribly important.
+
