2ebf220598a9c7d0e711ea41b4a31f2f8a2cf8b7
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-05-09 14:34:13 -0500

    Updating Ch 2 mostly

--- chapterFiles/rdmReview/biblioMetrixAnalysis/Untitled.R
+++
@@ -1,9 +0,0 @@
-# install.packages("textclean")
-require(textclean)
-
-
-# Function that takes potentially plural words and makes singular
-toSingular <- function(colname){
-  for(i in seq_along(colname))
-    
-}
