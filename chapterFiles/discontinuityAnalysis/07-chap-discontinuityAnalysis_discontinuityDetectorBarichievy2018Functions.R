571620e358b2436522d16bb45ac6776dd8aead0b
Jessica Burnett <TrashBirdEcology@users.noreply.github.com>
2019-05-18 18:15:14 -0500

    Add the barichievy 2018 functions and test them out on BBS data...
    blarg i don't want to do this stupid chapter!

---
+++ chapterFiles/discontinuityAnalysis/07-chap-discontinuityAnalysis_discontinuityDetectorBarichievy2018Functions.R
@@ -0,0 +1,66 @@
+## This script was adapted from the supplementary materials for the paper:  A method to detect discontinuities in census data (2018). Chris Barichievy*, David G. Angeler, Tarsha Eason, Ahjond S. Garmestani,  Kirsty L. Nash, Craig A. Stow, Shana Sundstrom, and Craig R. Allen.
+
+
+#1.Neutral.Null
+Neutral.Null <- function(log10.data, resolution = 4000) {
+  Dmax = max(log10.data, na.rm = FALSE)
+  Dmin = min(log10.data, na.rm = FALSE)
+  ds = (Dmax - Dmin) / resolution
+  MaxK = (Dmax - Dmin) / 2
+  MinK = ds * 2
+  #define h's to analyze
+  ks = seq(MinK, MaxK, by = 1 / resolution)
+  #generate matrix
+  bws = matrix(data = NA,
+               nrow = length(ks),
+               ncol = 1)
+  for (i in c(1:length(ks))) {
+    #calculate KS density estimate
+    KSdens <- density(log10.data, bw = ks[i], "gaussian", adjust = 1)
+    #Test if the ksdensity is unimodal
+    TF <- which(diff(sign(diff(KSdens$y))) == 2) + 1
+    if (length(TF) == 0)
+      bws[i] = 1
+    else
+      bws[i] = 0
+  }
+  #Define the neutral Null
+  r = min(which(bws == 1))
+  hnull = ks[r]
+  return(hnull)
+}
+
+#2. bootstrap function
+DD <- function(log10.data, hnull, Sample.N = 1000, adjust=1) {
+  NNull <- density(log10.data, bw = hnull, "gaussian", adjust = adjust)
+  N <- length(log10.data)
+  #generate matrix
+  
+  null.samples <- matrix(data = 0,
+                         ncol = Sample.N,
+                         nrow = N)
+  
+  for (i in 1:Sample.N) {
+    #sample the null model
+    rand.N <- sample(NNull$x, N, replace = TRUE, prob = NNull$y)
+    #calculate the gaps
+    null.samples[, i] <- sort(rand.N, decreasing = FALSE)
+    #put into the matrix
+  }
+  
+  #generate gaps
+  gaps.log10.data <- diff(log10.data)
+  gaps.null.samples <- diff(null.samples, decreasing = FALSE)
+  gap.percentile <- matrix(data = 0,
+                           nrow = length(gaps.log10.data),
+                           ncol = 1)
+  for (i in 1:length(gaps.log10.data)) {
+    #generate distribution of gaps per row (per gap rank)
+    gap.percentile[i] <-
+      ecdf(gaps.null.samples[i, ])(gaps.log10.data[i])
+    
+  }
+  Bootstrap.gaps <- rbind(gap.percentile, 0)
+  Bootstrap.gaps <- cbind(log10.data, Bootstrap.gaps)
+  return(Bootstrap.gaps)
+}
--- chapterFiles/discontinuityAnalysis/07-chap-discontinuity_analysis.R
+++ chapterFiles/discontinuityAnalysis/07-chap-discontinuity_analysis.R
@@ -1,9 +1,47 @@
+# Here is the data you requested. I've also attached the "constant power table" for you to reference. Column descriptions:
+# species = common name of bird species detected at a BBS route
+# log.mass = log-transformed mean body mass of species
+# d.value = value computed by "discontinuity detector" (i.e., Gap Rarity Index modified per Barichievy et al., 2018) for delineating discontinuities
+# aggs = body mass aggregation identified via "discontinuity detector." Low numbers indicate aggregations with small species (e.g., hummingbirds, etc).
+# Lati = latitude
+# Longi = longitude
+# Year = year of BBS survey
+
+# Setup -------------------------------------------------------------------
 # rm(list=ls())
-
 library(tidyverse)
-
 
 # Load and munge data -----------------------------------------------------
 data <- read.csv(here::here("chapterFiles/discontinuityAnalysis/cpr_DA_transectData.csv")) %>% 
   as_tibble()
+
+glimpse(data)
+
+unique(data$Longi) %>% length()
+unique(data$Lati) %>% length()
+
+
+pts <- data %>% distinct(Longi,Lati,Year) %>% 
+  group_by(Lati,Longi) %>% 
+  summarise(nYear= n_distinct(Year))
+
+head(pts)
+
+# Map ---------------------------------------------------------------------
+ggplot(pts, aes(Longi, Lati))+
+  geom_point(aes(size=nYear, color=as.factor(Lati)), show.legend=FALSE)+
+  geom_hline(aes(yintercept=(Lati)))
+
+
+# DD ----------------------------------------------------------------------
+source(here::here("/chapterFiles/discontinuityAnalysis/07-chap-discontinuityAnalysis_discontinuityDetectorBarichievy2018Functions.R"))
+
+test <- data %>% filter(Year==2010, 
+                        Lati == 48.28636,
+                        Longi == -93.55119)
+
+
+
+hnull<-Neutral.Null(test$log.mass,resolution)
+# Bootstrap.gaps<-DD(log10.data,hnull,Sample.N)
 
